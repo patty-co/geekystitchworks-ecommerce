949432d3a4502900d3e0ff16b69eb3abb7ea3b49
Patricia Costa <pattylife13@gmail.com>
2023-08-10 11:54:10 +0100

    add product page

--- types/ProductType.ts
+++ types/ProductType.ts
@@ -1,5 +1,13 @@
 export type ProductType = {
     name: string
-    price: number | null
+    unit_amount: number | null
+    quantity?: number | 1
     image: string
+    id: string
+    description: string | null
+    metadata: MetadataType
+}
+
+type MetadataType = {
+    features: string
 }

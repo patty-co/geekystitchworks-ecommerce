b56129721b7f1caa483ae65d8412ab35d048c649
Patricia Costa <pattylife13@gmail.com>
2023-08-13 11:12:14 +0100

    add and remove products from cart

--- app/components/Cart.tsx
+++ app/components/Cart.tsx
@@ -3,6 +3,7 @@
 import Image from "next/image"
 import { useCartStore } from "@/store"
 import formatPrice from "@/util/PriceFormat"
+import { IoAddCircle, IoRemoveCircle } from 'react-icons/io5'
 
 export default function Cart() {
     const cartStore = useCartStore()
@@ -18,7 +19,15 @@ export default function Cart() {
                         src={item.image} alt={item.name} height={120} width={120} />
                         <div>
                             <h2>{item.name}</h2>
-                            <h2>Quantity: {item.quantity}</h2>
+                            <div className="flex gap-2">
+                                <h2>Quantity: {item.quantity}</h2>
+                                <button onClick={() => cartStore.removeProduct({id: item.id, image: item.image, name: item.name, unit_amount: item.unit_amount, quantity: item.quantity})}>
+                                    <IoRemoveCircle/>
+                                </button>
+                                <button onClick={() => cartStore.addProduct({id: item.id, image: item.image, name: item.name, unit_amount: item.unit_amount, quantity: item.quantity})}>
+                                    <IoAddCircle/>
+                                </button>
+                            </div>
                             <p className="text-sm">{item.unit_amount && formatPrice(item.unit_amount)}</p>
                         </div>
                     </div>
--- store.ts
+++ store.ts
@@ -6,7 +6,8 @@ type CartState = {
     isOpen: boolean,
     cart: AddCartType[],
     toggleCart: () => void,
-    addProduct: (item: AddCartType) => void
+    addProduct: (item: AddCartType) => void,
+    removeProduct: (item: AddCartType) => void
 }
 
 export const useCartStore = create<CartState>()(
@@ -20,13 +21,28 @@ export const useCartStore = create<CartState>()(
                 if(existingItem) {
                     const updatedCart = state.cart.map((cartItem) => {
                         if(cartItem.id === item.id) {
-                            return {...cartItem, quantity: cartItem.quantity + 1}
+                            return {...cartItem, quantity: cartItem.quantity! + 1}
                         }
                         return cartItem
                     })
                     return { cart: updatedCart }
                 } else {
                     return { cart: [...state.cart, { ...item, quantity: 1 }] }
+                }
+            }),
+            removeProduct: (item) => set((state) => {
+                const existingItem = state.cart.find(cartItem => cartItem.id === item.id)
+                if(existingItem && existingItem.quantity! > 1) {
+                    const updatedCart = state.cart.map((cartItem) => {
+                        if(cartItem.id === item.id) {
+                            return {...cartItem, quantity: cartItem.quantity! - 1}
+                        }
+                        return cartItem
+                    })
+                    return { cart: updatedCart }
+                } else {
+                    const filteredCart = state.cart.filter((cartItem) => cartItem.id !== item.id)
+                    return { cart: filteredCart }
                 }
             })
         }),

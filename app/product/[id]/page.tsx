45ef64019ecbd73fcfada7d79fdc9275e8ff5667
Patricia Costa <pattylife13@gmail.com>
2023-08-13 06:35:04 +0100

    add products to cart

--- app/product/[id]/page.tsx
+++ app/product/[id]/page.tsx
@@ -1,6 +1,7 @@
 import Image from "next/image"
 import { SearchParamsTypes } from "@/types/SearchParamsTypes"
 import formatPrice from "@/util/PriceFormat"
+import AddCart from "./AddCart"
 
 export default async function Product({ searchParams }: SearchParamsTypes) {
     return(
@@ -15,7 +16,7 @@ export default async function Product({ searchParams }: SearchParamsTypes) {
                         {searchParams.unit_amount && formatPrice(searchParams.unit_amount)}
                     </p>
                 </div>
-                <button className="my-12 text-white py-2 px-6 rounded-md bg-teal-700">Add to cart</button>
+                <AddCart {...searchParams} />
             </div>
         </div>
     )

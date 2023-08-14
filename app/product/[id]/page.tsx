01ff86710566d2717315c55caf26b41667a0b569
Patricia Costa <pattylife13@gmail.com>
2023-08-14 06:51:45 +0100

    more mobile friendly

--- app/product/[id]/page.tsx
+++ app/product/[id]/page.tsx
@@ -5,8 +5,9 @@ import AddCart from "./AddCart"
 
 export default async function Product({ searchParams }: SearchParamsTypes) {
     return(
-        <div className="flex justify-between gap-24 p-12 text-gray-700">
-            <Image src={searchParams.image} alt={searchParams.name} width={600} height={600}/>
+        <div className="flex flex-col 2xl:flex-row items-center justify-between gap-24 text-gray-700">
+            <Image className="w-full"
+                src={searchParams.image} alt={searchParams.name} width={600} height={600}/>
             <div className="font-medium text-gray-700">
                 <h1 className="text-2xl py-2">{searchParams.name}</h1>
                 <p className="py-2">{searchParams.description}</p>
